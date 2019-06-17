04b7786e026aad79082e7ba8420d08ad7365922e
Stefano Bertoli <elkiwydev@gmail.com>
2019-06-17 23:21:01 +0200

    Added rough but functional subreddit selection view

--- src/homepage_native/shared/controllers/reddit.cljs
+++ src/homepage_native/shared/controllers/reddit.cljs
@@ -1,6 +1,6 @@
 (ns homepage-native.shared.controllers.reddit
   (:require [reagent.core :as r :refer [atom]]
-            [re-frame.core :refer [subscribe dispatch dispatch-sync]]
+            [re-frame.core :as rf]
             [homepage-native.shared.utils :as utils]
             [homepage-native.shared.style :as style]
             [homepage-native.shared.ui :as ui]
@@ -9,7 +9,11 @@
 
 
 
-(def test-url "https://www.reddit.com/r/clojure.json")
+(def reddit-base-url "https://www.reddit.com/r/")
+
+(def subSelectionYMin (* utils/sh 0.2))
+(def subSelectionYMax (* utils/sh 1.0))
+(def subSelectionY (ui/anim-new-value subSelectionYMax))
 
 
 
@@ -27,32 +31,56 @@
 
 
 
+(defn toggle-selection-view []
+    (ui/anim-set-value subSelectionY (if (= (ui/anim-get-value subSelectionY) subSelectionYMin) subSelectionYMax subSelectionYMin)))
 
 
 (defn title [name]
     (fn []
         [ui/view {:style {:border-bottom-width 2 :border-bottom-color @style/col-accent2}}
-            [ui/custom-header1 name]]))
+            [ui/custom-button-clear (str "r/" name) {:font-size 22} #(toggle-selection-view)]
+            ]))
 
 
 
+(defn subs-selection [item] ^{:key item}
+    (r/create-element
+        (r/reactify-component
+            (fn []
+                (let [subName (str (:item (js->clj item :keywordize-keys true)))]
+                    [ui/custom-button-clear subName {:color style/col-white} #(do (rf/dispatch-sync [:subreddit-selected-changed subName]) (toggle-selection-view))])))))
+
+
+
+
+(:children (:data @(rf/subscribe [:subreddit-selected-data])))
+(mapv #(name (first %)) (seq @(rf/subscribe [:subreddits])))
 
 
 (defn main-controller []
-    (let [subredditDataAtom (r/atom {})]
+    (let [subredditsAtom (rf/subscribe [:subreddits])
+          subredditDataAtom (rf/subscribe [:subreddit-selected-data])
+          subredditNameAtom (rf/subscribe [:subreddit-selected-name])]
         (fn []
-            (cond
-                (empty? @subredditDataAtom)
-                    (do (net/http-get-json test-url (fn [data] (reset! subredditDataAtom data)) )
-                       [ui/custom-header2 "Loading..."])
-            :else
-                (let [posts (:children (:data @subredditDataAtom))]
+            [ui/view 
+                (cond
+                    (empty? @subredditNameAtom)
+                        [ui/custom-header2 "No subreddit selected."]
 
+                    (empty? @subredditDataAtom)
+                        (do (net/http-get-json (str reddit-base-url @subredditNameAtom ".json") (fn [data] (rf/dispatch [:subreddit-fetched-data @subredditNameAtom data])) )
+                        [ui/custom-header2 "Loading..."])
 
-                    [ui/view  {:style {:height utils/sh}}
-                        [ui/flat-list {:data (clj->js posts) :render-item reddit-post :key-extractor (fn [item index] (str index))
-                                          :ListHeaderComponent (r/create-element (r/reactify-component (fn [] [title "Reddit"])))}]]
+                    :else
+                        (let [posts (:children (:data @subredditDataAtom))]
+                            [ui/view  {:style {:height utils/sh}}
+                                [ui/flat-list {:data (clj->js posts) :render-item reddit-post :key-extractor (fn [item index] (str index))
+                                               :ListHeaderComponent (r/create-element (r/reactify-component (fn [] [title @subredditNameAtom])))}]]
+                        )
+                )
 
-
-
-                    )))))
+                [ui/animated-view {:style {:backgroundColor style/col-black :position "absolute" :left 0 :top (:anim subSelectionY) :width utils/sw :height (* utils/sh 0.8)}}
+                    (let [subs (mapv #(name (first %)) (seq @subredditsAtom))]
+                        [ui/flat-list {:data subs
+                                       :render-item subs-selection
+                                       :key-extractor (fn [item index] (str index))}])]])))
