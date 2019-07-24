c0a1e3f3f2caf2319ccc78d5765c22722e3afb69
Stefano Bertoli <stefano.bertoli@sbsmobile.it>
2019-07-24 14:06:22 +0200

    Reorganized reddit.cljs and utils.cljs

--- src/homepage_native/shared/controllers/reddit.cljs
+++ src/homepage_native/shared/controllers/reddit.cljs
@@ -9,62 +9,81 @@
 
 
 
-(def reddit-base-url "https://www.reddit.com/r/")
+;; -----------------------------------------------------------------------------------------------------
+;; Costants
 
+(def reddit-base-url "https://www.reddit.com/r/")
 (def subSelectionYMin (* utils/sh 0))
 (def subSelectionYMax (* utils/sh 1.0))
-(defonce subSelectionY     (ui/anim-new-value subSelectionYMax))
-(defonce subSelectionAlpha (ui/anim-new-value 0))
-
-
-
-
-
-              ;:author "Mustardforyou"
-
-              ;:created 1554023315
-              ;:domain "discord.gg"
-
-              ;:downs 0
-              ;:ups 296
-              ;:score 296
-              ;:num_comments 49
-
-              ;:preview {:images [{:source {:url "https://external-preview.redd.it/T2UIMQ3uvC16xcd5gc6aCPeoOE7CdKnf-arERS5PCJA.jpg?auto=webp&amp;s=c422ea97905baf0b46680f61bca5b8480cccdc4b" :width 256 :height 256} :resolutions [{:url "https://external-preview.redd.it/T2UIMQ3uvC16xcd5gc6aCPeoOE7CdKnf-arERS5PCJA.jpg?width=108&amp;crop=smart&amp;auto=webp&amp;s=ac33d4801eaabbfe91931ee21ddedbbccf0493ab" :width 108 :height 108} {:url "https://external-preview.redd.it/T2UIMQ3uvC16xcd5gc6aCPeoOE7CdKnf-arERS5PCJA.jpg?width=216&amp;crop=smart&amp;auto=webp&amp;s=e1d02c0f58f919aaddced6a4bc50c34aeb2cc412" :width 216 :height 216}] :variants {} :id "_ea2YllHFhs3sRFzrQD4LPdD0xPtYXLYGi-oKu-6f3E"}] :enabled false}
-
-              ;:thumbnail "https://b.thumbs.redditmedia.com/dXAU8_8k9pxIySie4SsPBZ1U50OaHI1giiwm78TpaFY.jpg"
-              ;:thumbnail_height 140
-              ;:thumbnail_width 140
-
-              ;:title "Join the official /r/Meme Discord server!"
-              ;:url "https://discord.gg/GA7B7tg"
-              ;:permalink "/r/meme/comments/b7ia84/join_the_official_rmeme_discord_server/"
-
-
-
-
 (def post-image-height 100)
 (def post-image-margin 24)
 (def post-extra-height 100)
 
 
 
+;; -----------------------------------------------------------------------------------------------------
+;; Atoms
+
+(defonce subSelectionY     (ui/anim-new-value subSelectionYMax))
+(defonce subSelectionAlpha (ui/anim-new-value 0))
+
+
+
+;; -----------------------------------------------------------------------------------------------------
+;; Utility functions
+
+(defn toggle-selection-view []
+    (let [current (ui/anim-get-value subSelectionY)
+          toHide? (= current subSelectionYMin)]
+        (ui/anim-set-value subSelectionAlpha (if toHide? 0 0.95))
+        (ui/anim-set-value subSelectionY (if toHide? subSelectionYMax subSelectionYMin))))
+
+(defn get-first-subreddit []
+    (let [subreddits (rf/subscribe [:reddit-subreddits])]
+        (if (empty? @subreddits) "No subreddits." (first @subreddits))))
+
+
+
+;; -----------------------------------------------------------------------------------------------------
+;; Reagent components
+
+(defn settings-view []
+    (let [newSubName (r/atom "")
+          subreddits (rf/subscribe [:reddit-subreddits])
+          subToRemove (r/atom (get-first-subreddit))]
+        (fn []
+            [ui/view {:style {}}
+                [ui/custom-header1 "Reddit settings" {:color style/col-white}]
+
+                ;Add subreddit
+                [ui/custom-header2 "Add a subreddit" {:color style/col-white :margin-top 30}]
+                [ui/custom-text-input newSubName {} "subreddit-name"]
+                [ui/custom-button "Add" {:backgroundColor @style/col-accent2} #(rf/dispatch [:reddit-added-subreddit @newSubName])]
+
+                ;Remove fav
+                [ui/custom-header2 "Remove a subreddit" {:color style/col-white :margin-top 30}]
+                [ui/custom-selection-input subToRemove subreddits {}]
+                [ui/custom-button "Remove" {:backgroundColor @style/col-accent2} #(do (rf/dispatch-sync [:reddit-removed-subreddit @subToRemove])
+                                                                                      (reset! subToRemove (get-first-subreddit)))]])))
+
+
+
 (defn reddit-post [item] ^{:key item}
     (r/create-element
         (r/reactify-component 
-            (let [myRef   (r/atom nil)
-                  min-h   (r/atom 0)]
+            (let [myRef (r/atom nil)
+                  min-h (r/atom 0)]
                 (fn []
                     (let [info (:item (js->clj item :keywordize-keys true))
                           max-h      (+ @min-h post-image-height (* post-image-margin 2) post-extra-height)
                           animHeight (ui/anim-new-value @min-h)
                           animSep    (ui/anim-new-value 0)
                           textStyle  {:style (merge (style/style-text) {:flex 1 :padding 12})}
-                          {title :title
-                           url   :url
-                           thumbnail :thumbnail
-                           permalink :permalink
-                           selftext :selftext}  (:data info)]
+                          {title        :title
+                           url          :url
+                           thumbnail    :thumbnail
+                           permalink    :permalink
+                           selftext     :selftext}  (:data info)]
 
                         ;Main holder
                         [ui/view
@@ -85,14 +104,13 @@
                                 [ui/animated-view {:style {:backgroundColor @style/col-accent1 :width (:anim animSep) :height 1
                                                            :margin-left "auto" :margin-right "auto"}}]
 
-
                                 ;Post detail
                                 (let [image? (not (or (empty? thumbnail) (= thumbnail "self") (= thumbnail "default")))]
                                     [ui/view {:style {:flex-direction "row" :flex 1}}
                                         ;Image
                                         (when image?
-                                            [ui/image {:source {:uri thumbnail}
-                                                       :style {:borderWidth 2 :borderColor @style/col-accent2 :margin post-image-margin :width post-image-height :height post-image-height}}])
+                                            [ui/image {:source {:uri thumbnail} :style {:borderWidth 2 :borderColor @style/col-accent2
+                                                                                        :margin post-image-margin :width post-image-height :height post-image-height}}])
 
                                         ;Selftext / external url
                                         (if (empty? selftext)
@@ -121,17 +139,8 @@
                                                 ;Text
                                                 [ui/text {:style (merge (style/style-text style/col-white "200" 12)
                                                                         {:textAlign "justify" :margin post-image-margin :margin-left (if image? 0 post-image-margin)})}
-                                                    selftext]])])
+                                                    selftext]])])]]))))))
 
-                            ]]))))))
-
-
-
-(defn toggle-selection-view []
-    (let [current (ui/anim-get-value subSelectionY)
-          toHide? (= current subSelectionYMin)]
-        (ui/anim-set-value subSelectionAlpha (if toHide? 0 0.95))
-        (ui/anim-set-value subSelectionY (if toHide? subSelectionYMax subSelectionYMin))))
 
 
 (defn title [name]
@@ -152,39 +161,6 @@
                                  (toggle-selection-view))]])))))
 
 
-(defn get-first-subreddit []
-    (let [subreddits (rf/subscribe [:reddit-subreddits])]
-        (if (empty? @subreddits) "No subreddits." (first @subreddits))))
-
-
-
-(defn settings-view []
-    (let [newSubName (r/atom "")
-          subreddits (rf/subscribe [:reddit-subreddits])
-          subToRemove (r/atom (get-first-subreddit))]
-        (fn []
-            [ui/view {:style {}}
-
-                [ui/custom-header1 "Reddit settings" {:color style/col-white}]
-
-                ;Add subreddit
-                [ui/custom-header2 "Add a subreddit" {:color style/col-white :margin-top 30}]
-                [ui/custom-text-input newSubName {} "subreddit-name"]
-                [ui/custom-button "Add" {:backgroundColor @style/col-accent2} #(rf/dispatch [:reddit-added-subreddit @newSubName])]
-
-                ;Remove fav
-                [ui/custom-header2 "Remove a subreddit" {:color style/col-white :margin-top 30}]
-                [ui/custom-selection-input subToRemove subreddits {}]
-                [ui/custom-button "Remove" {:backgroundColor @style/col-accent2} #(do (rf/dispatch-sync [:reddit-removed-subreddit @subToRemove])
-                                                                                                              (reset! subToRemove (get-first-subreddit)))]])))
-
-
-
-;(str (:reddit @re-frame.db/app-db))
-;(rf/dispatch-sync [:reddit-added-subreddit "vim"])
-;(rf/dispatch-sync [:reddit-selected-changed "clojure"])
-
-
 
 (defn main-controller []
     (let [subredditsAtom (rf/subscribe [:reddit-subreddits])
@@ -195,7 +171,6 @@
                 (cond
                     (empty? @subredditNameAtom)
                         [title "Nothing"]
-                        ;[ui/custom-header2 "No subreddit selected."]
 
                     (empty? (get-in @subredditDataAtom [@subredditNameAtom]))
                         (do (net/http-get-json (str reddit-base-url @subredditNameAtom ".json")
@@ -205,11 +180,8 @@
                     :else
                         (let [posts (:children (:data (get @subredditDataAtom @subredditNameAtom )))]
                             [ui/view  {:style {:height (- utils/sh @ui/topInset )}}
-
-
                                 ;Title
                                 [title @subredditNameAtom]
-
                                 ;Post list
                                 [ui/flat-list {:data (filter #(not (nil? (:data %))) posts) :render-item reddit-post :key-extractor (fn [item index] (str index))}]]))
 
@@ -219,7 +191,7 @@
                                                                  :backgroundColor style/col-black-full :opacity (:anim subSelectionAlpha) }}]
                 [ui/animated-view {:style {:backgroundColor style/col-black :opacity 0.85 :position "absolute"
                                            :left 0 :top (:anim subSelectionY) :width utils/sw :height utils/sh}}
-                    [ui/flat-list {:data @subredditsAtom :render-item subs-selection :key-extractor (fn [item index] (str index))}]]
-                
+                    [ui/flat-list {:data @subredditsAtom :render-item subs-selection :key-extractor (fn [item index] (str index))}]]])))
 
-                    ])))
+
+
--- src/homepage_native/shared/utils.cljs
+++ src/homepage_native/shared/utils.cljs
@@ -4,19 +4,17 @@
 
 
 
-; ------------------------------------------------------------
-; REact object
 
+;; -----------------------------------------------------------------------------------------------------
+;; Main react object
 (def react (js/require "react-native"))
 
-; ------------------------------------------------------------
-; Utils
+;; -----------------------------------------------------------------------------------------------------
+;; Utils
 
 (def dimensions (.-Dimensions react))
 (def sw (.-width (.get dimensions "window")))
 (def sh (.-height (.get dimensions "window")))
-
-
 
 (defn discard-json
     "Takes the subreddits map and outputs the map without the jsons"
@@ -27,23 +25,17 @@
             (recur (rest subs)
                 (concat result [[(first (first subs)) {:json ""}]])))))
 
-
-
-
 (defn dissoc-in [m path key]
     (update-in m path dissoc key))
 
 (defn remove-from-vector [coll item]
     (into [] (remove #(= % item) coll)))
 
-
-
 (defn urlizeString [s]
     (if (nil? s) nil (clojure.string/replace s " " "%20")))
 
 (defn deurlizeString [s]
     (if (nil? s) nil (clojure.string/replace s "%20" " ")))
-
 
 (defn index [v pred]
     (ffirst (filter #(pred (second %)) (map-indexed vector v))))
